ba3636950f829f8c5b1e6461c6b889ed55ebdc9a
MiaKim0804 <kimmichino0804@gmail.com>
2022-06-07 16:50:24 +0800

    project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GoCurrency from './component/GoCurrency';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GoCurrency />
+  </React.StrictMode>
+);
+
+
